a6994e21ad6e659e7188d748392d734f97610e6d
unknown <footehud@gmail.com>
2017-08-01 17:31:32 -0400

    initial files and commenting

---
+++ basiccard.js
@@ -0,0 +1,9 @@
+//This file should define a Node module that exports a constructor for creating basic flashcards
+//move to the bottom?
+module.exports = basiccard.js; 
+
+//main constructor;  needs to accept two arguments 
+
+//contrusted object with front prpoerty with text on the front of the card
+
+//constrcuted object with back proeprty that contians text on back of the cards 
---
+++ clozecard.js
@@ -0,0 +1,10 @@
+//This file should define a Node module that exports a constructor for creating cloze-deleition flashcards
+//move to the bottom? needs js?
+module.exports = clozecard.js; 
+
+// o	The constructed object should have a cloze property that contains only the cloze-deleted portion of the text.
+// o	The constructed object should have a partial property that contains only the partial text.
+// o	The constructed object should have a fullText property that contains only the full text.
+
+
+
