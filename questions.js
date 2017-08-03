3b1b0dff67467200921e7757b990b060d31e8e5b
unknown <footehud@gmail.com>
2017-08-03 15:53:28 -0400

    addign info on npm hepburn link and scope question

--- questions.js
+++ questions.js
@@ -6,6 +6,7 @@ var BasicClozeCard = require("./clozecard.js");
 var firstPres = new BasicMainCard("Who was the first president of the United States", "George Washington");
 var taberu = new BasicMainCard("What is the meaning of the verb taberu?", "To eat");
 
+
 var chushingura = new BasicClozeCard("Chikamatsu Monzaemon", "...is the author of Goban Taiheiki", "Chikamatsu Monzaemon is the author of the Goban Takheiki");
 
 // // calling the two card methods on our sample [use a random quesiton?] object
@@ -26,3 +27,7 @@ console.log(chushingura.clozePart);
 console.log(chushingura.clozeRemove);
 
 // console.log(chushingura.fullText);
+
+
+/////////////////////RESOURCES
+//potentially run japanese text through node:  https://www.npmjs.com/package/hepburn
